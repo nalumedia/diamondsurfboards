7a2ae39cdb846550ef29d1a71ca17b1f0ccb2f8a
Ryan Dranginis <ryan.dranginis@gmail.com>
2015-06-04 22:39:18 -0700

    layout wack

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  get 'pages/diamond'
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
