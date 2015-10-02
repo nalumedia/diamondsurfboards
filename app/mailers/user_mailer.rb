a60e588dcfb3ff14367e902bb8ba22a6e32a352d
Ryan <ryan@nalumedia.com>
2015-10-02 08:58:37 -0700

    added d to email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ default from: "DiamondSurfboards.com <contact@diamondsurfboards.com>"
 
  def welcome_email(contact)
  @contact = contact
- mail(to: "ryan.dranginis@gmail.com", subject: 'Contact From Diamond Surfboards')
+ mail(to: "ryan.dranginis@gmail.com, diamondsurfboards@yahoo.com", subject: 'Contact From Diamond Surfboards')
 
 
   end
